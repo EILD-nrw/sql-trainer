75c0a6b0d72ce3f5f0254703e8fcac6dccef8b94
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-23 14:52:51 +0100

    Moved Database into TaskPage

--- src/Components/Pages/TaskPage.tsx
+++ src/Components/Pages/TaskPage.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// @ts-ignore
+// eslint-disable-next-line
+import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm" 
+import initSqlJs, { Database } from 'sql.js'
 
 export default function TaskPage () {
+  // Create Database
+  const [db, setDb] = useState<Database>()
+  useEffect(() => {
+    async function initDB () {
+      // sql.js needs to fetch its wasm file, so we cannot immediately instantiate the database
+      // without any configuration, initSqlJs will fetch the wasm files directly from the same path as the js
+      // see ../craco.config.js
+      try {
+        const SQL = await initSqlJs({ locateFile: () => sqlWasm })
+        setDb(new SQL.Database())
+        console.log(db?.exec('SELECT 1;'))
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    initDB()
+  }, [])
+
   return (
     <div>
 
