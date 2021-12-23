63f96b8cd529dd1b7f670d0881197fb53a8d47bc
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-23 15:26:04 +0100

    Added generic select element

---
+++ src/Components/UI/SelectElement.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+interface Props {
+  title: string
+  options: string[]
+  selected: string
+  setSelected: (x: string) => void
+}
+
+export default function SelectElement ({ title, options, selected, setSelected }: Props) {
+  function handleChange (e: React.ChangeEvent) {
+    const value = (e.target as HTMLInputElement).value
+    setSelected(value)
+  }
+
+  return (
+    <>
+      <label className='font-semibold' htmlFor="difficulty">{title}</label>
+      <select className='border rounded-sm' id="difficulty" value={selected} onChange={handleChange}>
+        {options.map(option => {
+          return <option key={option} value={option.toLowerCase()}>{option}</option>
+        })}
+      </select>
+    </>
+  )
+}
