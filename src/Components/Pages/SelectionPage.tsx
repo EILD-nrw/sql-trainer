e7a54a5758f2233d94ba2bb078f6c84a8aca733e
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-23 15:26:20 +0100

    Added schema and difficulty selection

--- src/Components/Pages/SelectionPage.tsx
+++ src/Components/Pages/SelectionPage.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PrimaryButton from '../UI/PrimaryButton'
+import SelectElement from '../UI/SelectElement'
 
 export default function SelectionPage () {
-  return (
-    <div>
+  const schemaOptions = ['Busse', 'Fahrrad', 'Reisen', 'Fußball', 'Welt']
+  const difficultyOptions = ['Leicht', 'Mittel', 'Schwer']
 
+  const [schema, setSchema] = useState('busse')
+  const [difficulty, setDifficulty] = useState('leicht')
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <h1 className="font-semibold text-2xl">Konfiguration</h1>
+      <p>Bitte wählen Sie ein Datenbankschema und einen Schwierigkeitsgrad aus:</p>
+      <SelectElement title="Schema" options={schemaOptions} selected={schema} setSelected={setSchema} />
+      <SelectElement title="Schwierigkeitsgrad" options={difficultyOptions} selected={difficulty} setSelected={setDifficulty} />
+      <PrimaryButton>Trainer Starten</PrimaryButton>
     </div>
   )
 }
