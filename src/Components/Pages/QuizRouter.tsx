705a1ce8efeaaf5b1eb1d44440e8bcc279a76557
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-23 16:54:33 +0100

    Added settings to localstorage

--- src/Components/Pages/QuizRouter.tsx
+++ src/Components/Pages/QuizRouter.tsx
@@ -3,9 +3,27 @@ import { Route, Routes } from 'react-router-dom'
 import SelectionPage from './SelectionPage'
 import TaskPage from './TaskPage'
 
+const LOCALSTORAGE_KEY_SCHEMA = 'selection.schema'
+const LOCALSTORAGE_KEY_DIFFICULTY = 'selection.difficulty'
+
 export default function QuizRouter () {
   const [schema, setSchema] = useState('busse')
   const [difficulty, setDifficulty] = useState('leicht')
+
+  // Restore settings if possible
+  useEffect(() => {
+    const lastSchema = window.localStorage.getItem(LOCALSTORAGE_KEY_SCHEMA)
+    if (lastSchema) setSchema(lastSchema)
+
+    const lastDifficulty = window.localStorage.getItem(LOCALSTORAGE_KEY_DIFFICULTY)
+    if (lastDifficulty) setDifficulty(lastDifficulty)
+  }, [])
+
+  // Store settings upon change
+  useEffect(() => {
+    window.localStorage.setItem(LOCALSTORAGE_KEY_SCHEMA, schema)
+    window.localStorage.setItem(LOCALSTORAGE_KEY_DIFFICULTY, difficulty)
+  }, [schema, difficulty])
 
   return (
     <Routes>
