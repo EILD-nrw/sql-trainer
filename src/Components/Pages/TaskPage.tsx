2af3ac1a398c0b204cf1d045b63ba1f1f152cc17
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-23 18:11:50 +0100

    Added editor to task page

--- src/Components/Pages/TaskPage.tsx
+++ src/Components/Pages/TaskPage.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import Editor from '@monaco-editor/react'
 
 // @ts-ignore
 // eslint-disable-next-line
 import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm" 
 import initSqlJs, { Database } from 'sql.js'
+import DetailsElement from '../UI/DetailsElement'
 
 interface Props {
   schema: string
@@ -11,7 +13,9 @@ interface Props {
 }
 
 export default function TaskPage ({ schema, difficulty }: Props) {
-  // Create Database
+  /*
+    Database
+  */
   const [db, setDb] = useState<Database>()
   useEffect(() => {
     async function initDB () {
@@ -29,9 +33,32 @@ export default function TaskPage ({ schema, difficulty }: Props) {
     initDB()
   }, [])
 
+  /*
+    Editor
+  */
+  const [code, setCode] = useState('')
+  function handleEditorChange (value: string | undefined) {
+    if (value) setCode(value)
+  }
+
+  /*
+    Output
+  */
+  const output = ''
+
   return (
-    <div>
-      {schema} {difficulty}
+    <div className='space-y-4'>
+      <h1 className='text-2xl font-semibold'>Trainer</h1>
+      <p className='font-semibold'>Welche Schauspieler ( Name, Wohnort) haben in Dramen von Schiller mitgespielt?</p>
+      <DetailsElement title='Editor'>
+        <Editor height='250px' language='sql' theme='vs-dark' value={code} onChange={handleEditorChange} options={{ minimap: { enabled: false } }} />
+      </DetailsElement>
+      {
+        output &&
+        <DetailsElement title='Ausgabe' startsOpen={false}>
+          {output}
+        </DetailsElement>
+      }
     </div>
   )
 }
