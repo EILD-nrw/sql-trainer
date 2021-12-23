29f9a3c93d4c9ed268606fff86869b0456824317
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-23 16:42:16 +0100

    Moved state up to routing component

--- src/Components/Pages/QuizRouter.tsx
+++ src/Components/Pages/QuizRouter.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SelectionPage from './SelectionPage'
 import TaskPage from './TaskPage'
 
 export default function QuizRouter () {
+  const [schema, setSchema] = useState('busse')
+  const [difficulty, setDifficulty] = useState('leicht')
+
   return (
     <Routes>
-      <Route path="selection" element={<SelectionPage />}></Route>
-      <Route path="tasks" element={<TaskPage />}></Route>
+      <Route path="selection" element={<SelectionPage schema={schema} setSchema={setSchema} difficulty={difficulty} setDifficulty={setDifficulty} />}></Route>
+      <Route path="tasks" element={<TaskPage schema={schema} difficulty={difficulty} />}></Route>
     </Routes>
   )
 }
