8705a3e156191690164bbf2ffa2402a8a12f8bae
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-23 18:24:07 +0100

    Now tracks its own state for better styling

--- src/Components/UI/DetailsElement.tsx
+++ src/Components/UI/DetailsElement.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props {
   children?: React.ReactNode
@@ -7,9 +7,16 @@ interface Props {
 }
 
 export default function DetailsElement ({ children, title, startsOpen = true }: Props) {
+  const [isOpen, setIsOpen] = useState(startsOpen)
+
+  function toggleDetails (e: React.MouseEvent) {
+    e.preventDefault()
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <details open={startsOpen} >
-      <summary className='font-semibold p-2 border rounded-t-lg bg-gray-400 border-gray-600 select-none cursor-pointer'>{title}</summary>
+    <details open={isOpen}>
+      <summary className={`font-semibold p-2 border ${isOpen ? 'rounded-t-lg' : 'rounded-lg'} bg-gray-400 border-gray-600 select-none cursor-pointer`} onClick={toggleDetails}>{title}</summary>
       {children}
     </details>
   )
