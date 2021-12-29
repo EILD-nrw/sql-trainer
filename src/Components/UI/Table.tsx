41ad9308ebca4d72f62ae896b6e485e73cf88689
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-12-29 03:53:13 +0100

    Now handles QueryExecResults

--- src/Components/UI/Table.tsx
+++ src/Components/UI/Table.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
+import { QueryExecResult } from 'sql.js'
 
 interface Props {
-  tableData: Object[]
+  tableData: QueryExecResult
 }
 
 export default function Table ({ tableData }: Props) {
+  const { columns, values } = tableData
+
   return (
     <div className="overflow-x-auto">
       <table className="shadow  w-full">
         <thead>
           <tr className="border">
-            {Object.keys(tableData[0]).map(key => {
+            {columns.map(key => {
               return <th className="bg-gray-100 border text-left px-4 py-2" key={key}>{key}</th>
             })}
           </tr>
         </thead>
         <tbody>
-          {tableData.map((row, index) => {
+          {values.map((row, index) => {
             return (
               <tr className={index % 2 === 1 ? 'bg-gray-50' : ''} key={index}>
-                {Object.values(row).map((entry, index) => {
+                {row.map((entry, index) => {
                   return <td className="border px-4 py-2" key={index}>{entry}</td>
                 })}
               </tr>
