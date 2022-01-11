9c01b67f571a46ba34ab51915b9abd6adbe85566
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2022-01-11 01:20:42 +0100

    Added first tables

---
+++ src/Tables.ts
@@ -0,0 +1,4 @@
+export default {
+  busse: ['Fuehrerscheinklassen', 'Mitarbeiter', 'Nichtbussfahrer', 'VAngestellte', 'Techniker', 'Bussfahrer', 'Busse', 'Einsatzplan', 'Inspektionen', 'Fahrten', 'Haltestelle', 'Verbindung', 'Linie', 'bestehen', 'Linie_Beschraenkungen', 'Beschraenkungen'],
+  fahrrad: ['KUNDEN', 'AUFTRAEGE', 'ANGESTELLTE', 'ABTEILUNGEN', 'GEH_KLASSEN', 'AUFTRAGSPOSITIONEN', 'ARTIKEL', 'ORTE', 'TEILE', 'STRUKTUR', 'WERKE', 'LAGER', 'LAGERBESTAND', 'LIEFERPROGRAMME', 'LIEFERUNGEN', 'LIEFERANTEN']
+}
