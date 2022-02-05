322288c0e3660cdda0de8deca5cac23121c412c4
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2022-02-05 17:27:18 +0100

    Fixed typo

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home () {
         Ziel dieses SQL-Trainers ist es, den Entwurf von <b>SELECT-Abfragen</b> einzuüben.
         <br/>
         Aus einem Pool von insgesamt über <b>300 Fragen</b> werden zufällig Aufgaben ausgewählt und vom Nutzer bearbeitet.
-        Die vom Nutzer geschriebenen Anfragen, werden direkt im Browser ausgeführt und mit einer Musterlösung verglichen.
+        Die vom Nutzer geschriebenen Anfragen, werden anschließend direkt im Browser ausgeführt und mit einer Musterlösung verglichen.
       </p>
       <p className="text-lg text-center">
         Hierzu wird im Browser selbst eine kleine <b>SQLite-Datenbank</b> betrieben in der die Aufgaben ausgeführt werden.
@@ -18,7 +18,7 @@ export default function Home () {
         Diese Datenbank versteht den großteil des SQL-Standards, den auch Oracle oder MySQL verwenden, hat aber gewisse Einschränkungen, beispielsweise bei der Verarbeitung von <i>Date</i> Typen.
       </p>
       <p className="text-lg text-center">
-        Wenn Sie die unterstützten Befehle Nachschlagen möchten, stellt <b>SQLite.org</b> eine <b><a href="https://www.sqlite.org/lang.html">entsprechende Übersicht</a></b> bereit.
+        Wenn Sie die unterstützten Befehle nachschlagen möchten, stellt <b>SQLite.org</b> eine <b><a href="https://www.sqlite.org/lang.html">entsprechende Übersicht</a></b> bereit.
       </p>
 
       <Link to="/sqltrainer/selection">
