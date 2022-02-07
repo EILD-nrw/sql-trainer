d186055321cf840db6697b0c76b7dbe77781fb7f
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2022-02-07 01:49:46 +0100

    Fixed error handling

--- src/Components/Pages/TaskPage.tsx
+++ src/Components/Pages/TaskPage.tsx
@@ -135,6 +135,10 @@ export default function TaskPage ({ schema, difficulty }: Props) {
     if (!db) return
     try {
       const execResults = db.exec(code)
+
+      if (execResults.length === 0) throw Error('No Results!')
+
+      console.log(execResults)
       setQueryData(execResults)
       evaluateQuery(execResults[0])
       setError('')
