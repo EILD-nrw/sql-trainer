442fce9a59b2bf025033fbf08421b042b4375a6a
Alexander Kosmehl <kosmehl@live.de>
2022-04-11 18:54:46 +0200

    Fixed fussball schema selection

--- src/Components/Pages/SelectionPage.tsx
+++ src/Components/Pages/SelectionPage.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 export default function SelectionPage ({ schema, setSchema, difficulty, setDifficulty }: Props) {
-  const schemaOptions = ['Busse', 'Fahrrad', 'Theater', 'Reisen', 'Fußball']
+  const schemaOptions = ['Busse', 'Fahrrad', 'Theater', 'Reisen', 'Fussball']
   const difficultyOptions = ['Leicht', 'Mittel', 'Schwer']
 
   return (
