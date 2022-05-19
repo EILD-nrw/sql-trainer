0300b3534c894661659501616a1f76bd50edb040
Alexander Kosmehl <kosmehl@live.de>
2022-05-19 14:01:34 +0200

    Applied prettier

--- src/Components/UI/DetailsElement.tsx
+++ src/Components/UI/DetailsElement.tsx
@@ -7,23 +7,37 @@ interface Props {
   taskSolved?: boolean
 }
 
-export default function DetailsElement ({ children, title, startsOpen = true, taskSolved = false }: Props) {
+export default function DetailsElement({
+  children,
+  title,
+  startsOpen = true,
+  taskSolved = false,
+}: Props) {
   const [isOpen, setIsOpen] = useState(startsOpen)
 
-  function toggleDetails (e: React.MouseEvent) {
+  function toggleDetails(e: React.MouseEvent) {
     e.preventDefault()
     setIsOpen(!isOpen)
   }
 
   return (
     <details open={isOpen}>
-      <summary className={`font-semibold p-2 border ${isOpen ? 'rounded-t-lg' : 'rounded-lg'} ${taskSolved ? 'bg-green-200' : 'bg-gray-200'} border-gray-600 select-none cursor-pointer`} onClick={toggleDetails}>
+      <summary
+        className={`font-semibold p-2 border ${
+          isOpen ? 'rounded-t-lg' : 'rounded-lg'
+        } ${
+          taskSolved ? 'bg-green-200' : 'bg-gray-200'
+        } border-gray-600 select-none cursor-pointer`}
+        onClick={toggleDetails}
+      >
         {title}
-        { taskSolved &&
-          <span className='font-bold'> - Richtig!</span>
-        }
+        {taskSolved && <span className="font-bold"> - Richtig!</span>}
       </summary>
-      <div className={`border-l border-r border-b rounded-b-lg border-gray-600 p-2 space-y-2 ${taskSolved ? 'bg-green-50' : ''}`}>
+      <div
+        className={`border-l border-r border-b rounded-b-lg border-gray-600 p-2 space-y-2 ${
+          taskSolved ? 'bg-green-50' : ''
+        }`}
+      >
         {children}
       </div>
     </details>
