91b415edc3950a53b6def7b25f61dc52e172edca
Alexander Kosmehl <kosmehl@live.de>
2022-05-19 13:44:59 +0200

    Added prettier support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error']
-  }
+    '@typescript-eslint/no-use-before-define': ['error'],
+  },
 }
