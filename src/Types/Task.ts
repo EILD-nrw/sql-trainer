b7f21ffe7301218a0c08c883d26954a1a244325d
Alexander Kosmehl <kosmehl@live.de>
2022-07-08 18:44:26 +0200

    Formatting

--- src/Types/Task.ts
+++ src/Types/Task.ts
@@ -1,8 +1,8 @@
 export interface Task {
-  id: number,
-  schema: string,
-  difficulty: 'leicht' | 'mittel' | 'schwer',
-  text: string,
-  solutionQuery: string,
+  id: number
+  schema: string
+  difficulty: 'leicht' | 'mittel' | 'schwer'
+  text: string
+  solutionQuery: string
   taskType: number
 }
